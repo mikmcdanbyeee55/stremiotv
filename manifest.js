4232c1a676575199ee01bad036d8428110c1a430
dantheman <80829378+mikmcdanbyeee55@users.noreply.github.com>
2023-09-30 04:53:56 +0100

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,13 @@
+module.exports={
+    id: "org.community.YourIPTV",
+    version: "1.1.1",
+    name: "IPTV DISCORD",
+    logo: "https://cdn.discordapp.com/icons/667359689780101130/596f5a89339ba7c31a873ff3559645bb.webp?size=160",
+    description: "Discord Your-IPTV",
+    types: ["movie", "series","tv","channel"],
+    background: "https://dl.strem.io/addon-background.jpg",
+    resources: ["movie","series","tv"],
+    catalogs: [],
+    idPrefixes: ["yiptv:"],
+    behaviorHints:{configurable : true, configurationRequired: true },
+};
